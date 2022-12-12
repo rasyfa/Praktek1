58eccb729bdb9c02e12bb871d39544e4539c85e3
rasyfa <rasyfa04@gmail.com>
2022-12-12 08:39:40 +0800

    Mengisi Nilai Atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -11,7 +11,11 @@ package praktek01;
  */
 public class MahasiswaAksi {
     public static void main(String[] args) {
+        Mahasiswa Harry = new Mahasiswa();
         
+        Harry.nama="Harry Potter";
+        Harry.NPM="19639999";
+        Harry.IPK=3.4;
     }
             
 }
